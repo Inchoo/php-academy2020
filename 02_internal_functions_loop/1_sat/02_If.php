6dcb63a09992e53e4f36eb918caaff9d6d4bf0ca
tnikcevs <tomislav.nikcevski@inchoo.net>
2020-08-28 13:58:21 +0200

    if adjustments

--- 02_internal_functions_loop/1_sat/02_If.php
+++ 02_internal_functions_loop/1_sat/02_If.php
@@ -1,14 +1,16 @@
 <?php
-// Conditional statements
+// Conditional statements - when we need to execute part of the code based on given condition
 // IF - basic syntax
 
 $i = 3;
 // Loose equal operator - equality (value comparison)
+// IF (variable $i equals 3) {execute this}
 if ($i == 3) {
     echo ' i equals 3 ';
 }
 
 // Strict equal operator - identity (both value and data type comparison - important)
+// IF (variable $i is identical to 3) {execute this}
 if ($i === 3) {
     echo ' i equals 3 ';
 }
@@ -18,19 +20,30 @@ if ($i === '3') {
     echo ' i equals 3 ';
 }
 
+// We can compare strings too
+$letter = 'abc';
+if ($letter == 'abc') {
+    echo ' Match! ';
+}
+
 // IF - full syntax
 $i = 4;
+// IF (variable $i is identical to 3) {execute this}
 if ($i === 3) {
     echo 'OK';
 } else {
+    // ELSE (in all other cases) {execute this}
     echo 'NOT OK';
 }
 
-// Nesting IFs (ifception - if's within if's)
+// Nesting IFs (if's within if's)
 $i = 2;
 //$i = 3;
 //$i = 4;
+
+// First level
 if ($i > 0) {
+    // Second level
     if ($i > 1) {
         echo 'Greater than 1';
     }
@@ -64,13 +77,12 @@ if ($i === 3)
     echo '45 OK';
 echo '46 OK';
 
-// Inline if (ternary operators) - used for cleaner presentation
+// Inline if (ternary operators) - used for cleaner presentation on one line
 echo $i === 3 ? ' 51 OK ' : ' 51 NOT ';
 
 
 // Alternative syntax for control structures
 // more on http://php.net/manual/en/control-structures.alternative-syntax.php
-
 ?>
 
 <?php if ($i === 3): ?>
@@ -89,3 +101,18 @@ echo $i === 3 ? ' 51 OK ' : ' 51 NOT ';
 //http://php.net/manual/en/control-structures.else.php
 //http://php.net/manual/en/control-structures.elseif.php
 //https://www.php-fig.org/psr/psr-2/
+
+<?php
+// Task, write this by changing only X,Y:
+$number = 13.2;
+// If $number: (is integer and greater than 5 and lesser than 20) OR (is string and greater than 8 and lesser than 18)
+// output "1"
+// If $number (is string)
+// output "2"
+// If $number (is NOT string)
+// output "3"
+// If $number (is greater than 5 and lesser than 20)
+// output "4"
+// If nothing above
+// output "5"
+?>
