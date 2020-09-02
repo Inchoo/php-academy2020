93a038e10f9c294d7c1fe115b8e5c2168dbba82f
Antun Matanović <antun.matanovic@gmail.com>
2020-09-02 19:36:57 +0200

    Creating script.sql separated by workflow 1/NA

---
+++ 04_db/01_script.sql
@@ -0,0 +1,10 @@
+create table student (
+    name text,
+    age int,
+    city text
+);
+/*
+Data types: https://dev.mysql.com/doc/refman/8.0/en/data-types.html
+Short explanation of the most common data types: https://dzone.com/articles/mysql-data-types-an-overview-of-the-data-types-in
+ */
+
