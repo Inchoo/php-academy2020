85143e8f051cbad4b2fff2cd31007dc4d1023b89
Antun Matanović <antun.matanovic@gmail.com>
2020-09-02 19:40:24 +0200

    Creating script.sql separated by workflow 3/NA

--- 04_db/01_script.sql
+++ 04_db/01_script.sql
@@ -10,3 +10,4 @@ Data types: https://dev.mysql.com/doc/refman/8.0/en/data-types.html
 Short explanation of the most common data types: https://dzone.com/articles/mysql-data-types-an-overview-of-the-data-types-in
  */
 
+insert into student (name, age, city) values ('Iva Ivić', 25, 'Osijek'); /* Error Incorrect string value: '\xC4\x87' for column 'name' at row 1 */
