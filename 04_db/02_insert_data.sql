fb01459eda3885deec01032c2a66826ad28e339c
Antun Matanović <antun.matanovic@gmail.com>
2020-09-07 14:32:20 +0200

    Creating script.sql separated by workflow 14/NA

--- 04_db/02_insert_data.sql
+++ 04_db/02_insert_data.sql
@@ -8,7 +8,8 @@ values
     ('Snježana', 'Snježić', 25, 1),
     ('Ante', 'Antić', 20, 3),
     ('Tena', 'Tenić', 22, 3),
-    ('Suzana', 'Suzić', 33, 4);
+    ('Suzana', 'Suzić', 33, 4)
+    ('Iva', 'Ivančić', 25, 3);
 
 insert into lecture (name, ects)
 values
@@ -29,6 +30,7 @@ values
 (1, 3, NULL),
 (2, 1, 2),
 (2, 4, NULL),
-(3, 7, NULL),
+(3, 7, 4),
+(3, 1, 2),
 (4, 5, 4),
 (4, 9, 4);
